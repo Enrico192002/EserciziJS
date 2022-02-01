e2e0dd6f2fdfa7eb31dfa744ffc36c7c70470b2a
Enrico192002 <94382848+Enrico192002@users.noreply.github.com>
2022-02-01 18:37:26 +0100

    Delete 2022_02_01 directory

--- 2022_02_01/hello.js
+++
@@ -1,5 +0,0 @@
-let p = [ {x:0.0, y:12.0 }, { x:5.0, y:0.0} ]
-console.log("ben ritrovati iniziamo javascipt");
-d = math.sqrt( (p[0].x - p[1].x) ** 2 +
-             (p[0].y- p[1].y) ** 2 )
-console.log("il risultato è " + d)
